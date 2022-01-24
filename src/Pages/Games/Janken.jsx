95076305e956a44a43842fae0101d463ff771fde
Yannis <Yannis@DESKTOP-M7S462D>
2022-01-24 23:14:31 +0100

    update janken

--- src/Pages/Games/Janken.jsx
+++ src/Pages/Games/Janken.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import {ButtonGroup, Typography} from "@mui/material";
@@ -7,6 +7,15 @@ import Button from "@mui/material/Button";
 const url = "https://raw.githubusercontent.com/tomjamesallen/blackjack-react/master/src/public/img/cards";
 
 export default function Janken() {
+    const [userHit, setUserHit] = useState("default");
+    const [computerHit, setComputerHit] = useState("default");
+
+    function play(hit) {
+        setUserHit(hit);
+        // request api;
+        //setComputerHit();
+    }
+
   return (
       <Grid>
         <Box>
@@ -15,11 +24,11 @@ export default function Janken() {
         <Grid container flexDirection="row" justifyContent="space-around">
           <Box item>
             <Typography textAlign="center" variant="h5">Joueur</Typography>
-            <img src={`${url}/s-3.svg`} width="150" height="180"/>
+            <img src={"/src/assets/janken/" + userHit + ".png"} width="150" height="180"/>
           </Box>
           <Box item>
             <Typography textAlign="center" variant="h5">Ordinateur</Typography>
-            <img src={`${url}/s-6.svg`} width="150" height="180"/>
+            <img src={"/src/assets/janken/" + computerHit + ".png"} width="150" height="180"/>
           </Box>
         </Grid>
         <Box container>
@@ -30,9 +39,9 @@ export default function Janken() {
           <Typography textAlign="center">Veuillez choisir votre coup</Typography>
           <Box justifyContent="center" container>
             <ButtonGroup variant="contained" aria-label="outlined primary button group">
-              <Button>Pierre</Button>
-              <Button>Feuille</Button>
-              <Button>Ciseau</Button>
+              <Button onClick={() => play("rock")} >Pierre</Button>
+              <Button onClick={() => play("paper")} >Feuille</Button>
+              <Button onClick={() => play("scissors")} >Ciseau</Button>
             </ButtonGroup>
           </Box>
         </Grid>
