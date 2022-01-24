9c71e6b8055a3a494d24de629413a42e28eae4e5
Yannis <Yannis@DESKTOP-M7S462D>
2022-01-24 21:00:21 +0100

    remove file

--- src/api/Bataille.js
+++
@@ -1,8 +0,0 @@
-
-
-export default function Bataille() {
-    // return Deck[] each deck contain 26 cards soit un total de 52 cartes l'ensemble de ces cartes sont différentes
-    function getDecks() {
-
-    }
-}
