7fbe7bc1f52d6227d37cace76c62e3eb291f7e75
Yannis <Yannis@DESKTOP-M7S462D>
2022-01-24 18:11:54 +0100

    add info on home page to avoid "there are a problem"

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,18 +1,17 @@
 import * as React from 'react';
 import Grid from "@mui/material/Grid";
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
 
 
 export default function Home() {
     return (
-        <Grid sx={{
-            backgroundImage: 'url(https://mir-s3-cdn-cf.behance.net/project_modules/fs/b38e4925095973.56f57df922fac.jpg)',
-            backgroundRepeat: 'no-repeat',
-            backgroundColor: (t) =>
-                t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-        }}>
+        <Grid>
+            <Typography>home</Typography>
 
+            <Link href="Games" variant="body2">
+                {"Jeux"}
+            </Link>
         </Grid>
     )
 }
