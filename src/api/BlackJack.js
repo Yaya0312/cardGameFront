12895ede0de78d2f9d329f15c87ec0151070abfd
Yannis <Yannis@DESKTOP-M7S462D>
2022-01-24 21:01:54 +0100

    add file blackjack for api (no completed)

---
+++ src/api/BlackJack.js
@@ -0,0 +1,23 @@
+
+
+export default function BlackJack() {
+
+    // Demande une carte au serveur
+    function hit() {
+
+    }
+
+    // Indique au serveur que le joueur à fini
+    // response :
+    // - les cartes du croupiers
+    // - le gagnant (1: dealer, 2 player)
+    // - le score Actuel ou le score final si le nombre de round à été défini
+    function stand() {
+
+    }
+
+    // demande un nouveau tour
+    function newTurn() {
+
+    }
+}
