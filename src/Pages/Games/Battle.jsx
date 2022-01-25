82823e3263814e26eec3e4384215876841e51f5d
Yannis <Yannis@DESKTOP-M7S462D>
2022-01-25 19:46:02 +0100

    fix equality

--- src/Pages/Games/Battle.jsx
+++ src/Pages/Games/Battle.jsx
@@ -2,28 +2,36 @@ import Grid from "@mui/material/Grid";
 import {Typography} from "@mui/material";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 const url = "https://raw.githubusercontent.com/tomjamesallen/blackjack-react/master/src/public/img/cards";
 export default function Battle() {
-    let maxRound = 3;
     let [currentRound, setCurrentRound] = useState(0);
     let [playerScore, setPlayerScore] = useState(0);
     let [computerScore, setComputerScore] = useState(0);
-    let cardsUser = [5,6,8];
-    let cardComputer = [5,3,8];
+    const [result,setResult] = useState("");
+    let cardsUser = [5,6,8,2,2];
+    let cardComputer = [5,3,8,9,4];
+    const maxRound = cardsUser.length;
 
     function play() {
         if (currentRound === maxRound) {
             return;
         }
         setCurrentRound(currentRound + 1);
+    }
+
+    useEffect(() => {
         if (cardsUser[currentRound-1] > cardComputer[currentRound-1]) {
             setPlayerScore(playerScore + 1);
+            setResult("Vous avez gagné la manche");
         } else if (cardsUser[currentRound-1] < cardComputer[currentRound-1]) {
             setComputerScore(computerScore + 1);
+            setResult("Vous avez perdu la manche");
+        } else {
+            setResult("égalité");
         }
-    }
+    }, [currentRound]);
 
     const currentCard = (deck) => currentRound === 0 ? "back" : "s-" + deck[currentRound-1];
 
@@ -31,6 +39,7 @@ export default function Battle() {
         <Grid>
             <Box>
                 <Typography textAlign="center" variant="h3">Round {currentRound}/{maxRound}</Typography>
+                <Typography textAlign="center" variant="h5">{result}</Typography>
             </Box>
             <Grid container flexDirection="row" justifyContent="space-around">
                 <Box item>
