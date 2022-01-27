90754f1601d827aede458ab027a0fc56e4bdd00f
Yannis <Yannis@DESKTOP-M7S462D>
2022-01-27 22:37:13 +0100

    delete useless file

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -3,7 +3,6 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import {Link} from "react-router-dom";
 
-
 export default function GameCard(props) {
     return (
         <Card sx={{ maxWidth: 345 }}>
